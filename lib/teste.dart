c2d733c5d0e223e488a5f804af5a7d0a448c7a69
ikyrie <mah.c5r@gmail.com>
2024-12-09 14:36:13 -0300

    video3.2

--- lib/teste.dart
+++ lib/teste.dart
@@ -1,7 +1,10 @@
+import 'package:climapp/app/adpaters/http_adapter.dart';
 import 'package:climapp/app/services/weather_service.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
-  final WeatherService service = WeatherService();
+  final IHttpAdapter client = HttpClient(http.Client());
+  final WeatherService service = WeatherService(client);
   final result = await service.getWeather("Toledo");
   print(result.cityName);
 }

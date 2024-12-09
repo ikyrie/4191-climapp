ca1ea6e83e9cb5bb1b81902b513f28874dcbaccf
ikyrie <mah.c5r@gmail.com>
2024-12-09 14:58:55 -0300

    video3.3

---
+++ lib/app/injection/injection_container.dart
@@ -0,0 +1,11 @@
+import 'package:climapp/app/adpaters/http_adapter.dart';
+import 'package:climapp/app/services/weather_service.dart';
+import 'package:http/http.dart' as http;
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerLazySingleton<IHttpAdapter>(() => HttpClient(http.Client()));
+  getIt.registerLazySingleton<WeatherService>(() => WeatherService(getIt.get<IHttpAdapter>()));
+}

fda1a234597750a0e20972d7b52d1c4924bf7929
ikyrie <mah.c5r@gmail.com>
2024-12-09 12:00:50 -0300

    video2.1

--- lib/app/services/weather_service.dart
+++ lib/app/services/weather_service.dart
@@ -1,16 +1,17 @@
 // lib/services/weather_service.dart
 import 'dart:convert';
 import 'package:climapp/app/api_key.dart';
+import 'package:climapp/app/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
   final String baseUrl = 'https://api.hgbrasil.com/weather';
 
-  Future<Map<String, dynamic>> getWeather(String city) async {
+  Future<WeatherModel> getWeather(String city) async {
     final response = await http.get(Uri.parse('$baseUrl?key=$apiKey&city_name=$city&unit=metric'));
 
     if (response.statusCode == 200) {
-      return json.decode(response.body);
+      return WeatherModel.fromJson(json.decode(response.body)['results']);
     } else {
       throw Exception('Failed to load weather data');
     }
